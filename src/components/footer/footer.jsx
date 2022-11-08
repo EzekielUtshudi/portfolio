b84288e8153dfd2ea8ce805c7b3f65242ba4d26b
Ezekielutshudi <ezekielutshudi@gmail.com>
2022-11-08 16:16:13 +0200

    footer content added are : -Styling of the footer -add socials link

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './footer.css'
+import { FiFacebook } from 'react-icons/fi'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { AiFillLinkedin } from 'react-icons/ai'
+import { FiTwitter } from 'react-icons/fi'
+
 
 
 function footer() {
@@ -18,11 +23,18 @@ function footer() {
       </ul>
 
       <div className='footer__socials'>
-        <a href='https://www.facebook.com/ezekiel.utshudi/' target={'_blank'}></a>
-        <a href='https://www.instagram.com/ezekielutshudi/' target={'_blank'}></a>
-        <a href='https://www.linkedin.com/in/ezekiel-utshudi-eteta-9b1b1b1b3/' target={'_blank'}></a>
-        <a href='twitter.com/ezekielutshudi' target={'_blank'}></a>
+        <a href='https://www.facebook.com/ezekiel.utshudi/' target={'_blank'}><FiFacebook/></a>
+        <a href='https://www.instagram.com/ezekielutshudi/' target={'_blank'}><AiOutlineInstagram/></a>
+        <a href='https://www.linkedin.com/in/ezekiel-utshudi-eteta-9b1b1b1b3/' target={'_blank'}><AiFillLinkedin/></a>
+        <a href='https://www.twitter.com/ezekielutshudi' target={'_blank'}><FiTwitter/></a>
       </div>
+
+      <div className='footer__copyright'>
+         <small>
+            © 2022 Ezekiel Utshudi. All rights reserved.
+         </small>
+      </div>
+
     </footer>
   )
 }
